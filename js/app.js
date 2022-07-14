5e988e471a8333726ea16a4837817494d647378f
Junyoung Son <junyoungson808@gmail.com>
2022-07-14 14:34:36 -0700

    Fixed function wraps -Jun

--- js/app.js
+++ js/app.js
@@ -3,80 +3,80 @@
 let numberOfCorrectAnswers = 0;
 let siteVisitorName = prompt('What is your name?');
 
-alert(`Welcome, and thanks for visiting ${siteVisitorName}! Please guess these facts about me.`);
+// alert(`Welcome, and thanks for visiting ${siteVisitorName}! Please guess these facts about me.`);
 
-function questionOnee(){
-  let questionOne = prompt('Yes or No... Am I from Washington State?').toLowerCase();
+// function questionOnee(){
+//   let questionOne = prompt('Yes or No... Am I from Washington State?').toLowerCase();
 
-  if(questionOne === 'y' || questionOne === 'yes'){
-    alert('I wish I was!');
-    numberOfCorrectAnswers++;
-  } else if(questionOne === 'n' || questionOne === 'no'){
-    alert('I am one of those former Californians who traded palm trees for pine trees.');
-  } else{
-    alert('Answer yes or no');
-  }
-}
-questionOnee();
+//   if(questionOne === 'y' || questionOne === 'yes'){
+//     alert('I wish I was!');
+//     numberOfCorrectAnswers++;
+//   } else if(questionOne === 'n' || questionOne === 'no'){
+//     alert('I am one of those former Californians who traded palm trees for pine trees.');
+//   } else{
+//     alert('Answer yes or no');
+//   }
+// }
+// questionOnee();
 
-function questionTwoo(){
+// function questionTwoo(){
 
-  let questionTwo = prompt('Yes or No... Do I use an umbrella?').toLowerCase();
+//   let questionTwo = prompt('Yes or No... Do I use an umbrella?').toLowerCase();
 
-  if(questionTwo === 'y' || questionTwo === 'yes'){
-    alert('I am embarassed to admit it... but sometimes I do. I know that makes me a bad Washingtonian.');
-    numberOfCorrectAnswers++;
-  } else if(questionTwo === 'n' || questionTwo === 'no'){
-    alert('I know this makes me a bad Washingtonian... but sometimes I do.');
-  } else{
-    alert('Answer yes or no');
-  }
-}
-questionTwoo();
+//   if(questionTwo === 'y' || questionTwo === 'yes'){
+//     alert('I am embarassed to admit it... but sometimes I do. I know that makes me a bad Washingtonian.');
+//     numberOfCorrectAnswers++;
+//   } else if(questionTwo === 'n' || questionTwo === 'no'){
+//     alert('I know this makes me a bad Washingtonian... but sometimes I do.');
+//   } else{
+//     alert('Answer yes or no');
+//   }
+// }
+// questionTwoo();
 
-function questionThreee(){
+// function questionThreee(){
 
-  let questionThree = prompt('Yes or No... Do I believe in bigfoot?').toLowerCase();
+//   let questionThree = prompt('Yes or No... Do I believe in bigfoot?').toLowerCase();
 
-  if(questionThree === 'y' || questionThree === 'yes'){
-    alert('I believe in a lot of things, but I am not team bigfoot yet.');
-    numberOfCorrectAnswers++;
-  } else if(questionThree === 'n' || questionThree === 'no'){
-    alert('Maybe one day... I can imagine sparkly vampires and moody werewolves roaming the forest, but bigfoot feels like a stretch.');
-  } else{
-    alert('Answer yes or no');
-  }
-}
-questiongThreee();
+//   if(questionThree === 'y' || questionThree === 'yes'){
+//     alert('I believe in a lot of things, but I am not team bigfoot yet.');
+//     numberOfCorrectAnswers++;
+//   } else if(questionThree === 'n' || questionThree === 'no'){
+//     alert('Maybe one day... I can imagine sparkly vampires and moody werewolves roaming the forest, but bigfoot feels like a stretch.');
+//   } else{
+//     alert('Answer yes or no');
+//   }
+// }
+// questionThreee();
 
-function questionFourr(){
+// function questionFourr(){
 
-  let questionFour = prompt('Yes or No... Do I drink coffee?').toLowerCase();
+//   let questionFour = prompt('Yes or No... Do I drink coffee?').toLowerCase();
 
-  if(questionFour === 'y' || questionFour === 'yes'){
-    alert('You will always see me at the start of class with a oversized mug full of coffee.');
-    numberOfCorrectAnswers++;
-  } else if(questionFour === 'n' || questionFour === 'no'){
-    alert('I quit coffee for a while and turned to herbal tea but have since reunited with my true love.');
-  } else{
-    alert('Answer yes or no');
-  }
-}
-questionFourr();
+//   if(questionFour === 'y' || questionFour === 'yes'){
+//     alert('You will always see me at the start of class with a oversized mug full of coffee.');
+//     numberOfCorrectAnswers++;
+//   } else if(questionFour === 'n' || questionFour === 'no'){
+//     alert('I quit coffee for a while and turned to herbal tea but have since reunited with my true love.');
+//   } else{
+//     alert('Answer yes or no');
+//   }
+// }
+// questionFourr();
 
-function questionFivee(){
-  let questionFive = prompt('Yes or No... Do I like to camp?').toLowerCase();
+// function questionFivee(){
+//   let questionFive = prompt('Yes or No... Do I like to camp?').toLowerCase();
 
-  if(questionFive === 'y' || questionFive === 'yes'){
-    alert(`Thanks for viewing me as that type of person ${siteVisitorName}... but I like indoor plumbing too much.`);
-    numberOfCorrectAnswers++;
-  } else if(questionFive === 'n' || questionFive === 'no'){
-    alert(`It sounds nice in theory ${siteVisitorName}... but I like my hobbit hole too much to leave the Shire.`);
-  } else{
-    alert('Answer yes or no');
-  }
-}
-questionFivee();
+//   if(questionFive === 'y' || questionFive === 'yes'){
+//     alert(`Thanks for viewing me as that type of person ${siteVisitorName}... but I like indoor plumbing too much.`);
+//     numberOfCorrectAnswers++;
+//   } else if(questionFive === 'n' || questionFive === 'no'){
+//     alert(`It sounds nice in theory ${siteVisitorName}... but I like my hobbit hole too much to leave the Shire.`);
+//   } else{
+//     alert('Answer yes or no');
+//   }
+// }
+// questionFivee();
 
 function questionSixx(){
 
@@ -107,9 +107,9 @@ questionSixx();
 
 function questionSevenn(){
 
-  guessCount = 6;
+  let guessCount = 6;
   let favoriteCharacterNames = ['R2D2', 'GROGU', 'YODA', 'BABY YODA'];
-  correctGuess = false;
+  let correctGuess = false;
   let questionSeven;
 
   while (guessCount && !correctGuess){
